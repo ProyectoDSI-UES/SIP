cd344dd886cc95d735f20568d651012364b09653
Alexis-Prz21 <mp21030@ues.edu.sv>
2024-10-15 19:06:43 -0600

    Eliminación lógico de roles

--- roles/eliminar_roles.php
+++ roles/eliminar_roles.php
@@ -2,17 +2,22 @@
 
 include('../includes/conn.php');
 
-          if(isset($_REQUEST['id_rol']))
-            {
-              $id_rol=$_REQUEST['id_rol'];
-            }
-            else
-            {
-            $id_rol=$_POST['id_rol'];
-          }
+// Verificando si el ID viene por GET o POST
+if (isset($_REQUEST['id_rol'])) {
+  $id_rol = $_REQUEST['id_rol'];
+} else {
+  $id_rol = $_POST['id_rol'];
+}
 
-  mysqli_query($conn,"delete from roles where id_rol='$id_rol'")or die(mysqli_error($conn));
-    echo "<script type='text/javascript'>alert('Eliminado correctamente!');</script>";
-  echo "<script>document.location='roles.php'</script>";  
-  
-?>
+$sql = "UPDATE roles SET estado = 0 WHERE id_rol='$id_rol'";
+$query = mysqli_query($conn, $sql) or die(mysqli_error($conn));
+
+if ($query) {
+  // Mensaje de éxito
+  echo "<script type='text/javascript'>alert('Eliminado correctamente!');</script>";
+} else {
+  echo "<script type='text/javascript'>alert('Error al eliminar');</script>";
+}
+
+// Redirigiendo a la página de roles
+echo "<script>document.location='roles.php'</script>";
