d1a6de07569a89221bf3066b50f799f3066ed6c7
Alexis-Prz21 <mp21030@ues.edu.sv>
2024-11-20 14:37:00 -0600

    Creación del módulo para registrar y procesar solicitudes de vacaciones

---
+++ gestion_vacacion/aprobar.php
@@ -0,0 +1,16 @@
+<?php session_start();
+
+include('../includes/conn.php');
+
+// Verificando si el ID viene por GET o POST
+if (isset($_REQUEST['id'])) {
+    $id_solicitud = $_REQUEST['id'];
+} else {
+    $id_solicitud = $_POST['id_solicitud'];
+}
+
+$sql = "UPDATE solicitudes_vacaciones SET estado = 'Aprobado' WHERE id_solicitud='$id_solicitud'";
+$query = mysqli_query($conn, $sql) or die(mysqli_error($conn));
+
+// Redirigiendo a la página de plaza
+echo "<script>document.location='gestion_vacacion.php'</script>";
